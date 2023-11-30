714ddf1654a5f0cd062b681670b85e0f8d66d835
Jin <jinsh1110927@naver.com>
2023-11-30 13:49:56 +0900

    display update

--- app/src/main/java/com/example/dailywater/MainActivity.java
+++ app/src/main/java/com/example/dailywater/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.target_edit);
+        setContentView(R.layout.main_page_1);
     }
 }
