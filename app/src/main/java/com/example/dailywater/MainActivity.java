e6fa257ed5769bd9c67b00272eaa08eadf275df8
hennible0612 <sungwoo166@gmail.com>
2023-11-30 15:16:32 +0900

    데이터베이스 생성

--- app/src/main/java/com/example/dailywater/MainActivity.java
+++ app/src/main/java/com/example/dailywater/MainActivity.java
@@ -1,40 +1,76 @@
 package com.example.dailywater;
 
+import static java.sql.DriverManager.println;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        Button buttonOpenCustomTarget = findViewById(R.id.button_open_custom_target);
-        buttonOpenCustomTarget.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CustomTargetActivity.class);
-                startActivity(intent);
-            }
-        });
 
+        createDatabase();
+        createTables();
+    }
 
-        Button buttonOpenCustomTarget1 = findViewById(R.id.button_open_achievement_rage);
-        buttonOpenCustomTarget1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AchievementRateActivity.class);
-                startActivity(intent);
-            }
-        });
+    private void createDatabase() {
+        try {
+            db = openOrCreateDatabase(
+                    "HYDRATE_DB",
+                    Activity.MODE_PRIVATE,
+                    null);
 
-        setContentView(R.layout.main_page_1);
+            println("database is created.");
+
+            dropAllTables(db);
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            println("database is not created.");
+        }
+    }
+    private void createTables() {
+        db.execSQL("CREATE TABLE IF NOT EXISTS DailyResult ("
+                + " _id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + " date DATE NOT NULL, "
+                + " success INTEGER NOT NULL CHECK (success IN (0,1)) DEFAULT 0, "
+                + " water_drank INTEGER NOT NULL DEFAULT 0);");
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS ToDo ("
+                + " _id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + " activity_name TEXT NOT NULL, "
+                + " activity_status INTEGER NOT NULL CHECK (activity_status IN (0,1)) DEFAULT 0, "
+                + " water_reward INTEGER NOT NULL);");
+    }
+
+    private void insertDummyData() {
 
     }
+
+    private void dropAllTables(SQLiteDatabase db) {
+        try {
+            String[] tables = {"DailyResult", "ToDo"};
+
+            for (String table : tables) {
+                db.execSQL("DROP TABLE IF EXISTS " + table);
+            }
+            println("All tables are dropped.");
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            println("Error in dropping tables.");
+        }
+    }
+
 }
